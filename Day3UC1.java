acacf9e96acf5b14b59a23bfcb02572a5f6dc8dd
06kaushik <himanshu06kaushik@gmail.com>
2021-04-15 14:21:15 +0530

    Calculate length

---
+++ Day3UC1.java
@@ -0,0 +1,11 @@
+public class LengthOfLineUC1 {
+
+		public static void main(String[] args) {
+		//variables
+		int x1=1, y1=2, x2=4, y2=5;
+		//Computation
+		double length = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+		System.out.println(length);
+	}
+}
+© 2021 GitHub, Inc.
